7e5351d87f0c1bf0197a86d8c79c83a80ac4a28f
Colm Carew <colmcarew2@gmail.com>
2017-01-23 22:15:40 +0000

    Coffee Error Message

--- app/src/main/java/ie/cm/activities/Add.java
+++ app/src/main/java/ie/cm/activities/Add.java
@@ -2,6 +2,7 @@ package ie.cm.activities;
 
 import ie.cm.models.Coffee;
 import ie.cm.R;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,50 +13,49 @@ import android.widget.RatingBar;
 import android.widget.Toast;
 
 public class Add extends Base implements
-		OnClickListener {
+        OnClickListener {
 
-	private String 		coffeeName, coffeeShop;
-	private double 		coffeePrice, ratingValue;
-	private EditText 	name, shop, price;
-	private RatingBar 	ratingBar;
+    private String coffeeName, coffeeShop;
+    private double coffeePrice, ratingValue;
+    private EditText name, shop, price;
+    private RatingBar ratingBar;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.add);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-		Button saveButton = (Button) findViewById(R.id.saveCoffeeBtn);
-		name = (EditText) findViewById(R.id.nameEditText);
-		shop = (EditText) findViewById(R.id.shopEditText);
-		price = (EditText) findViewById(R.id.priceEditText);
-		ratingBar = (RatingBar) findViewById(R.id.coffeeRatingBar);
-		saveButton.setOnClickListener(this);
-	}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add);
 
-	public void onClick(View v) {
-		
-		coffeeName = name.getText().toString();
-		coffeeShop = shop.getText().toString();
-		try {
-			coffeePrice = Double.parseDouble(price.getText().toString());
-		} catch (NumberFormatException e) {
-			coffeePrice = 0.0;
-		}
-		ratingValue = ratingBar.getRating();
+        Button saveButton = (Button) findViewById(R.id.saveCoffeeBtn);
+        name = (EditText) findViewById(R.id.nameEditText);
+        shop = (EditText) findViewById(R.id.shopEditText);
+        price = (EditText) findViewById(R.id.priceEditText);
+        ratingBar = (RatingBar) findViewById(R.id.coffeeRatingBar);
+        saveButton.setOnClickListener(this);
+    }
 
-		if ((coffeeName.length() > 0) && (coffeeShop.length() > 0)
-				&& (price.length() > 0)) {
-			Coffee c = new Coffee(coffeeName, coffeeShop, ratingValue,
-					coffeePrice, false);
+    public void onClick(View v) {
 
-			coffeeList.add(c);
-			goToActivity(this,Home.class, null);
-		} else
-			Toast.makeText(
-					this,
-					"You must Enter Something for "
-							+ "\'Name\', \'Shop\' and \'Price\'",
-					Toast.LENGTH_SHORT).show();
-	}
+        coffeeName = name.getText().toString();
+        coffeeShop = shop.getText().toString();
+        try {
+            coffeePrice = Double.parseDouble(price.getText().toString());
+        } catch (NumberFormatException e) {
+            coffeePrice = 0.0;
+        }
+        ratingValue = ratingBar.getRating();
+
+        if ((coffeeName.length() > 0) && (coffeeShop.length() > 0)
+                && (price.length() > 0)) {
+            Coffee c = new Coffee(coffeeName, coffeeShop, ratingValue,
+                    coffeePrice, false);
+
+            coffeeList.add(c);
+            goToActivity(this, Home.class, null);
+        } else {
+            String coffeeAddIssue = getString(R.string.coffeAddIssue);
+            Toast.makeText(this, coffeeAddIssue, Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
